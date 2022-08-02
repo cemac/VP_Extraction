Changes added to average over fixed altitude intervals in order to reduce spatial sampling scale.
Smoothing also fixed to sort end point errors.

vp_functions.py
@@ -137,58 +137,51 @@ def savitzky_golay(y, window_size, order, deriv=0, rate=1):
 
 def smooth(x,window_len=11,window='hanning'):
     """smooth the data using a window with requested size.
-
+    
     This method is based on the convolution of a scaled window with the signal.
-    The signal is prepared by introducing reflected copies of the signal
-    (with the window size) in both ends so that transient parts are minimized
-    in the begining and end part of the output signal.
-
+    
+    
     input:
-        x: the input signal
+        x: the input signal 
         window_len: the dimension of the smoothing window; should be an odd integer
         window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
             flat window will produce a moving average smoothing.
 
     output:
         the smoothed signal
-
+        
     example:
 
     t=linspace(-2,2,0.1)
     x=sin(t)+randn(len(t))*0.1
     y=smooth(x)
-
-    see also:
-
+    
+    see also: 
+    
     numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
     scipy.signal.lfilter
-
+ 
     TODO: the window parameter could be the window itself if an array instead of a string
     NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
     """
 
     if x.ndim != 1:
-        raise (ValueError, "smooth only accepts 1 dimension arrays.")
+        raise ValueError("smooth only accepts 1 dimension arrays.")
 
     if x.size < window_len:
-        raise (ValueError, "Input vector needs to be bigger than window size.")
-
+        raise ValueError("Input vector needs to be bigger than window size.")
 
     if window_len<3:
         return x
 
 
     if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
-        raise (ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
+        raise ValueError("Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
 
 
-    s=np.r_[x[window_len-1:0:-1],x,x[-2:-window_len-1:-1]]
-    if window == 'flat': #mDLI200513132229.RAWZK42oving average
-        w=np.ones(window_len,'d')
-    else:
-        w=eval('np.'+window+'(window_len)')
-
-    y=np.convolve(w/w.sum(),s,mode='valid')
+    box = np.ones(window_len)/window_len
+    y = np.convolve(x, box, mode='same')
+    y[0] = x[0]# to remove jumps at the lowest elevation
     return y
 
 
@@ -388,8 +381,8 @@ def get_r_indexes(r, steps_in_range_delta, max_range,verbose = True):
 	r_indxs.extend(to_r)
 	return(r_indxs)
 
-
-def altitude_parameter_averaging_cvp_static(radar, field, cvp_index, avg_range_delta, azimuth_exclude = None, verbose=True):
+##Neely
+def altitude_parameter_averaging_cvp_static(radar, field, cvp_index, avg_range_delta, azimuth_exclude = None, min_h = None, max_h = None, h_step = None, verbose=True):
 
     [r,az,el] = map(int, cvp_index)
 
@@ -457,7 +450,20 @@ def altitude_parameter_averaging_cvp_static(radar, field, cvp_index, avg_range_d
     # get altitudes for the column values
     column_altitudes = altitudes[np.ix_(range(0,radar.nsweeps),az_indxs,r_indxs)] #13.12.2021 test
     #column_altitudes = altitudes[np.ix_(range(0,100),az_indxs,r_indxs)]
-
+    #get the voxel size for the altitude levels
+    
+    #### Neely
+    base = 5 # precision
+    
+    if h_step is None: h_step = int(1000 * round(((az_size)/1000))) 
+    
+    if min_h is None: min_h = int(1000 * base * round((np.nanmin(altitudes)/1000)/base))
+    
+    if max_h is None: max_h = int(1000 * base * round((np.nanmax(altitudes)/1000)/base))
+    
+    equidistant_alt = np.linspace((min_h + h_step/2), (max_h - h_step/2), num=int(max_h/h_step))
+    equidistant_bound = np.linspace((min_h), (max_h), num=int(max_h/h_step)+1)
+    ####
     if verbose:
 	    print ("!!!! column_altitudes.shape")
 	    print (column_altitudes.shape)
@@ -492,66 +498,44 @@ def altitude_parameter_averaging_cvp_static(radar, field, cvp_index, avg_range_d
         rhohv = rhohv_3D_data[np.ix_(range(0,radar.nsweeps),az_indxs,r_indxs)]
         (meteoMask) = kdpfun.generate_meteo_mask(elev, rays, bins, flags, rhohv, METEO_THRESH)
         column = kdpfun.unwrap_phidp(elev, rays, bins, meteoMask, column)
+    
+    ###Neely
+    # get mean of means for equidistant
+    equdist_mean = np.zeros(equidistant_alt.shape[0])*np.nan
+    equdist_std = np.zeros(equidistant_alt.shape[0])*np.nan
+    equdist_count = np.zeros(equidistant_alt.shape[0])*np.nan
+    equdist_total = np.zeros(equidistant_alt.shape[0])*np.nan
+    
+    for item, boundary  in enumerate(equidistant_bound[0:-1]):
 
-    if verbose:
-        print ("column.shape is {}".format(column.shape) )
-    #summed = np.zeros(column.shape)
-    counted = np.zeros(column.shape)
-    mask = np.ma.masked_invalid(column).mask
-    inv_mask = np.where(mask, 0, 1)
-    #summed = np.nansum(column, axis=1)
-    counted = np.nansum(inv_mask, axis=1)
-    observation_count = np.where(counted == 0, np.nan, counted)
-    mean_values = np.nanmean(column, axis=1)
-    std_values = np.nanstd(column, axis=1)
+        if(item == len(equidistant_bound)-1):bin_indexes = np.where(column_altitudes>=boundary)
+        else:bin_indexes = np.where((column_altitudes>=boundary) & (column_altitudes<equidistant_bound[item+1]))
+
+        equdist_mean[item] = np.nanmean(column[bin_indexes])
+        equdist_std[item] = np.nanstd(column[bin_indexes])
+        equdist_count[item] = len(np.isfinite(column[bin_indexes]))
+        equdist_total[item] = len(column[bin_indexes])
+        
+    if len(equidistant_alt) >= 300: win = 5
+    else:win = 3
+    
+    smoothed_alt_level_mean = smooth(equdist_mean,win)	
+
+    ####
+        
 
     if mask_field in ['dBuZ', 'dBZ', 'dBZ_ac', 'dBuZv', 'dBZv']:
-        mean_values = 10 * np.log10(mean_values)
-        std_values = 10 * np.log10(std_values)
+        equdist_mean = 10 * np.log10(equdist_mean)
+        equdist_std = 10 * np.log10(equdist_std)
 
-    # flattened mean array
-    flat_means = mean_values.flatten().reshape((np.prod(mean_values.shape),1))
-
-    # flattened std array
-    flat_std = std_values.flatten().reshape((np.prod(std_values.shape),1))
-    flat_count = observation_count.flatten().reshape((np.prod(observation_count.shape),1))
-    output_2D_array = []
-    output_2D_array = np.append(flat_altitudes, flat_means, axis=1)
-    output_2D_array = np.append(output_2D_array, flat_std, axis=1)
-    output_2D_array = np.append(output_2D_array, flat_count, axis=1)
-
-    sorted_by_altitude_output_array = output_2D_array[output_2D_array[:,0].argsort()]
-    tmp_array = sorted_by_altitude_output_array
-
-    masked_sorted_by_altitude_output_array = sorted_by_altitude_output_array[~np.isnan(sorted_by_altitude_output_array).any(axis=1)]
-    masked_sorted_by_altitude_output_array = masked_sorted_by_altitude_output_array[~np.isinf(masked_sorted_by_altitude_output_array).any(axis=1)]
-
-    if(len(masked_sorted_by_altitude_output_array)>0):
-        #x = np.linspace(np.min(masked_sorted_by_altitude_output_array[:,0]),np.max(masked_sorted_by_altitude_output_array[:,0]),10000)
-        bin_means, bin_edges, binnumber = stats.binned_statistic(masked_sorted_by_altitude_output_array[:,0],masked_sorted_by_altitude_output_array[:,1], statistic='mean', bins=np.linspace(0,10000,num=101))#bins=np.linspace(0,5000,num=51))
-        bin_std, bin_edges, binnumber = stats.binned_statistic(masked_sorted_by_altitude_output_array[:,0],masked_sorted_by_altitude_output_array[:,1], statistic='std', bins=np.linspace(0,10000,num=101))#bins=np.linspace(0,10000,num=101))
-        bin_count, bin_edges, binnumber = stats.binned_statistic(masked_sorted_by_altitude_output_array[:,0],masked_sorted_by_altitude_output_array[:,1], statistic='count', bins=np.linspace(0,10000,num=101))#bins=np.linspace(0,10000,num=101))
-        bin_width = (bin_edges[1] - bin_edges[0])
-        bin_centers = bin_edges[1:] - bin_width/2
-        #win=5
-        #smoothed_mean = smooth(bin_means,win)
-
-    else:
-        bin_means, bin_edges, binnumber = stats.binned_statistic(tmp_array[:,0],tmp_array[:,1], statistic='mean', bins=np.linspace(0,10000,num=101))#bins=np.linspace(0,5000,num=51))
-        bin_std, bin_edges, binnumber = stats.binned_statistic(tmp_array[:,0],tmp_array[:,1], statistic='std', bins=np.linspace(0,10000,num=101))#bins=np.linspace(0,10000,num=101))
-        bin_count, bin_edges, binnumber = stats.binned_statistic(tmp_array[:,0],tmp_array[:,1], statistic='count', bins=np.linspace(0,10000,num=101))#bins=np.linspace(0,10000,num=101))
-        bin_width = (bin_edges[1] - bin_edges[0])
-        bin_centers = bin_edges[1:] - bin_width/2
-        bin_means = bin_means * np.nan
-        bin_std = bin_std * np.nan
 
     timeofsweep = num2date(np.nanmean(radar.time['data'][:]),
                                        radar.time['units'],
                                        radar.time['calendar'])
-
-    zero_array = np.zeros((column.shape[1],))
-    zero_array[:] = np.nan
-    return bin_centers, bin_count, bin_means, bin_std, timeofsweep
+    
+    ### Neely
+    return equidistant_alt, equdist_count, smoothed_alt_level_mean, equdist_std, timeofsweep
+    #return bin_centers, bin_count, bin_means, bin_std, timeofsweep
 
 
 def add_dim(arr, arr_name, verbose=False):
@@ -666,8 +650,8 @@ def altitude_parameter_averaging_qvp(radar, elevation, field, azimuth_exclude, v
 
             return (altitudes, observation_count, mean_values, std_values, timeofsweep)
 
-
-def time_height(list_of_files, field_list, cvp_indexes = None, avg_range_delta = 5, elevation = None, count_threshold=0,  met_office=False, azimuth_exclude = [], verbose=False, vp_mode='qvp'):
+###Neely Change time_height_cvp_static
+def time_height(list_of_files, field_list, cvp_indexes = None, avg_range_delta = 5, elevation = None, count_threshold=0,  met_office=False, azimuth_exclude = [], verbose=False, vp_mode='qvp',min_h = None, max_h = None, h_step = None):
 
     import itertools
 
@@ -725,7 +709,7 @@ def time_height(list_of_files, field_list, cvp_indexes = None, avg_range_delta =
                     (alts, counts, means, standard_deviations, timeofsweep) = \
                         altitude_parameter_averaging_cvp_static(radar, field,
                             cvp_index, avg_range_delta,
-                            azimuth_exclude = azimuth_exclude, verbose=verbose)
+                            azimuth_exclude = azimuth_exclude, min_h=min_h, max_h=max_h, h_step=h_step, verbose=verbose)
                 elif vp_mode == 'cvp_dynamic':
                     (alts, counts, means, standard_deviations, timeofsweep) = \
                         altitude_parameter_averaging_cvp(radar, field,